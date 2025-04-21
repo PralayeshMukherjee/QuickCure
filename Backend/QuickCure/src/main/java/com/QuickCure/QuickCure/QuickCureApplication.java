e5dbe060a41a75cefeca3c26dc34455de2546000
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-04-21 21:26:17 +0530

    initiate the backend configuration of this table

---
+++ Backend/QuickCure/src/main/java/com/QuickCure/QuickCure/QuickCureApplication.java
@@ -0,0 +1,13 @@
+package com.QuickCure.QuickCure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickCureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickCureApplication.class, args);
+	}
+
+}
---
+++ Backend/QuickCure/src/test/java/com/QuickCure/QuickCure/QuickCureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QuickCure.QuickCure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickCureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
